612fb9a0b17f1e2e23eaf4e4721e0f34d97b68f7
gopisettu <gopisettu3011@gmail.com>
2026-07-31 10:50:38 +0530

    added REDUX  react-reducre and redux-toolkit and created store.js and Action and Reducer package and CardReduce.js initilizes with cart[] empty store and added at store.js

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
 <BrowserRouter>
 <App />
-</BrowserRouter>    
+</BrowserRouter> 
+</Provider>   
   
 )
